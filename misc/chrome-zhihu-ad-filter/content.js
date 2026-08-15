b18a4b23f2858c6a2b52552e8b337cf81b7c6e23
zhuoyi233 <zhuoyi233@yeah.net>
2026-08-15 17:20:15 +0800

    chore(harmony): 移除 dev 分支安卓项目残留（A/B 类，含 workflow/idea/gitignore 清理）

--- misc/chrome-zhihu-ad-filter/content.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const script = document.createElement('script');
-  script.src = chrome.runtime.getURL('injected.js');
-  script.type = 'text/javascript';
-  script.async = false;
-
-  (document.head || document.documentElement).appendChild(script);
-  script.remove();
-})();
